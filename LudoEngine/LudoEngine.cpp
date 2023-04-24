07550f3a91121556d35c7fe63d74080e88bd0040
Amrit Choudhary <amrit.choudhary@jetsynthesys.com>
2023-04-24 12:58:18 +0530

    SDL added to the project.

--- LudoEngine/LudoEngine.cpp
+++ LudoEngine/LudoEngine.cpp
@@ -1,17 +1,45 @@
-#include <iostream>
-#include "Source/LudoManager.h"
+#include <stdlib.h>
 
-int main()
-{
-	LudoEngine::LudoManager ludoManager;
-	int i = 0;
-	int maxTurns = 100;
-	
-	while (i < maxTurns) {
-		i++;
-		ludoManager.PlayTurn();
+#include <SDL/SDL.h>
+#include <SDL/SDL_image.h>
+
+int main(int argc, char** argv) {
+	SDL_Event event;
+	SDL_Renderer* renderer = NULL;
+	SDL_Texture* texture = NULL;
+	SDL_Texture* texture2 = NULL;
+
+	SDL_Window* window = NULL;
+
+	SDL_Init(SDL_INIT_TIMER | SDL_INIT_VIDEO);
+	SDL_CreateWindowAndRenderer(
+		720, 720,
+		0, &window, &renderer
+	);
+
+	IMG_Init(IMG_INIT_PNG);
+	texture = IMG_LoadTexture(renderer, "Resources/CellBG.png");
+	texture2 = IMG_LoadTexture(renderer, "Resources/blue_piece_trans.png");
+
+	while (1) {
+		SDL_Rect rect { 360, 360, 50, 50 };
+		SDL_Rect rect2 { 360, 360, 30, 30 };
+
+		SDL_RenderCopy(renderer, texture, NULL, &rect);
+		SDL_RenderCopy(renderer, texture2, NULL, &rect2);
+		
+		
+		SDL_RenderPresent(renderer);
+		if (SDL_PollEvent(&event) && event.type == SDL_QUIT)
+			break;
 	}
 
-	char c = std::cin.get();
-}
 
+
+	SDL_DestroyTexture(texture);
+	IMG_Quit();
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+	return EXIT_SUCCESS;
+}
